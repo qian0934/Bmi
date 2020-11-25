885b53d144a381358afc489d7f8b3a694b0a035c
Strong <qian19921225@gmail.com>
2020-11-25 18:56:33 +0800

    linearlayout,onclick

--- app/src/main/java/com/strong/bmi/MainActivity.kt
+++ app/src/main/java/com/strong/bmi/MainActivity.kt
@@ -2,10 +2,20 @@ package com.strong.bmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ed_weight
+    }
+    fun bmi(view: View){
+        val weight = ed_weight.text.toString().toFloat()
+        val height = ed_hight.text.toString().toFloat()
+        val bmi = weight / (height*height)
+        Log.d("BMI",bmi.toString())
     }
 }
